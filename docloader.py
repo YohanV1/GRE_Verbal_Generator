65d2cdaa072d8b574a6cb23edd490b5876da58c8
Yohan Vinu <yohanvvinu@gmail.com>
2024-08-15 15:39:13 +0530

    Initial commit

---
+++ docloader.py
@@ -0,0 +1,9 @@
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+
+DATA_PATH = "data"
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
